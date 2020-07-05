82cb1d9be2e7f3af11505c7518bcf62485c77e52
Karan Singh <ksingh1@cs.iitr.ac.in>
2020-07-05 13:32:30 +0530

    logo

--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -25,7 +25,7 @@ class Header extends Component{
               <Navbar light style={{backgroundColor:"#5a7698" , height:"100px"}} expand="md">
                   <div className="container">
                       <NavbarToggler onClick={this.toggleNav} />
-                      <NavbarBrand className="mr-auto" href="/" >
+                      <NavbarBrand className="mr-auto" href="/home" >
                           <img src="logo192.png" height="30" width="41" alt="logo" /> 
                       </NavbarBrand>
                       <Collapse isOpen={this.state.isNavOpen} navbar>
